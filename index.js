171030cd8c1ed9f99e6e454de316e99f86cc40c0
JakubSkraly <leonelinga18@outlook.es>
2022-08-07 03:25:18 -0500

    V2.0 del Encriptador

--- index.js
+++ index.js
@@ -3,33 +3,86 @@ var texto_final = document.getElementById("texto_final");
 texto_final.readOnly = true;
 var original = ['e', 'i', 'a', 'o', 'u'];
 var reemplazo = ['enter', 'imes', 'ai', 'ober', 'ufat'];
+var toast_texto = document.getElementById("toast_texto");
+const my_toast = document.getElementById('toast');
+
+function mostrar_div() {
+    var sin_mensaje = document.getElementById("sin_mensaje");
+    var mensaje = document.getElementById("mensaje");
+    sin_mensaje.style.display = "none";
+    mensaje.style.display = "";
+    // sin_mensaje.setAttribute("style", "display: none;");
+    // mensaje.setAttribute("style", "display: block;");
+}
 
 function encriptar() {
-    if (/^[a-z\s]*$/.test(texto.value)) {
-        var texto_encriptado = texto.value;
-        for (var i = 0; i < original.length; i++) {
-            texto_encriptado = texto_encriptado.replace(new RegExp(original[i], 'g'), reemplazo[i]);
-        }
-        texto_final.innerHTML = texto_encriptado;
+    if (texto.value.length == 0 || texto.value.trim() == "") {
+        my_toast.classList.remove('text-bg-success');
+        my_toast.classList.add('text-bg-danger');
+        const toast = new bootstrap.Toast(my_toast);
+        toast_texto.innerHTML = "<i class='bi bi-exclamation-octagon' style='font-size: 20px;'></i> &nbsp;&nbsp;Por favor ingrese un texto";
+        toast.show()
     } else {
-        alert("Solo letras minusculas, sin caracteres especiales");
+        if (/^[a-z\s]*$/.test(texto.value)) {
+            mostrar_div();
+            var texto_encriptado = texto.value;
+            for (var i = 0; i < original.length; i++) {
+                texto_encriptado = texto_encriptado.replace(new RegExp(original[i], 'g'), reemplazo[i]);
+            }
+            texto_final.innerHTML = texto_encriptado;
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Upss...',
+                text: 'Solo letras minusculas, sin acentos y sin caracteres especiales',
+                allowEnterKey: false,
+                showCancelButton: true,
+                cancelButtonText: 'OK',
+                showConfirmButton: false,
+            })
+        }
     }
 }
 
 function desencriptar() {
-    if (/^[a-z\s]*$/.test(texto.value)) {
-        var texto_desencriptado = texto.value;
-        for (var i = 0; i < reemplazo.length; i++) {
-            texto_desencriptado = texto_desencriptado.replace(new RegExp(reemplazo[i], 'g'), original[i]);
-        }
-        texto_final.innerHTML = texto_desencriptado;
+    if (texto.value.length == 0 || texto.value.trim() == "") {
+        my_toast.classList.remove('text-bg-success');
+        my_toast.classList.add('text-bg-danger');
+        const toast = new bootstrap.Toast(my_toast);
+        toast_texto.innerHTML = "<i class='bi bi-exclamation-octagon' style='font-size: 20px;'></i> &nbsp;&nbsp;Por favor ingrese un texto";
+        toast.show()
     } else {
-        alert("Solo letras minusculas, sin caracteres especiales");
+        if (/^[a-z\s]*$/.test(texto.value)) {
+            mostrar_div();
+            var texto_desencriptado = texto.value;
+            for (var i = 0; i < reemplazo.length; i++) {
+                texto_desencriptado = texto_desencriptado.replace(new RegExp(reemplazo[i], 'g'), original[i]);
+            }
+            texto_final.innerHTML = texto_desencriptado;
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Upss...',
+                text: 'Solo letras minusculas, sin acentos y sin caracteres especiales',
+                allowEnterKey: false,
+                showCancelButton: true,
+                cancelButtonText: 'OK',
+                showConfirmButton: false,
+            })
+        }
     }
 }
 
 function copiar() {
-    texto_final.select();
+    var aux = document.createElement("input");
+    aux.setAttribute("value", texto_final.innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
     document.execCommand("copy");
-    alert("Texto copiado");
+    document.body.removeChild(aux);
+    Swal.fire({
+        icon: 'success',
+        text: 'Texto copiado al portapapeles',
+        allowEnterKey: false,
+    })
 }
